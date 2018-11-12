a921c1aa154d6959009438206127a8636cb84ec7
Anastasija Pischik <nastijpischik@gmail.com>
2018-11-12 07:46:59 +0300

    Добавьте файлы проекта.

---
+++ StopWatch/GCD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StopWatch
+{
+    class GCD
+    {
+        public int Algorithm1(int n1, int n2)
+        {
+            while (n1 != 0 && n2 != 0)
+                if (n1 > n2)
+                    n1 = n1 % n2;
+                else
+                    n2 = n2 % n1;
+            return n1 + n2;
+        }
+
+        public int Algorithm2(int n1, int n2)
+        {
+            if (n2 == 0)
+                return n1;
+            return Algorithm2(n2, n1 % n2);
+        }
+    }
+}
---
+++ StopWatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            GCD gcd = new GCD();
+            int res;
+            sw.Start();
+            res = gcd.Algorithm1(72,27);
+            sw.Stop();
+            Console.WriteLine(res);
+            Console.WriteLine(sw.Elapsed);
+            sw.Reset();
+            sw.Start();
+            res = gcd.Algorithm2(10, 30);
+            sw.Stop();
+            Console.WriteLine(res);
+            Console.WriteLine(sw.Elapsed);      
+            Console.ReadLine();
+        }
+    }
+}
